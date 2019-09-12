4cef67dce122ace9fef886bf157e9ef190b69bda
harsh patel <hkp27299@gmail.com>
2019-09-12 14:01:44 +0530

    Update options.dart

--- lib/src/options.dart
+++ lib/src/options.dart
@@ -10,205 +10,208 @@ class OptionsPage extends StatelessWidget {
     var appWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      body: Container(
-        width: appWidth,
-        height: appHeight,
-        child: Column(
-          children: <Widget>[
-            Container(
-              child: Text(
-                "Choose a method",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: appWidth / 12,
-                  fontWeight: FontWeight.w600,
+      body: SingleChildScrollView(
+        child: Container(
+          width: appWidth,
+          height: appHeight,
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Text(
+                  "Choose a method",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: appWidth / 12,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
+                margin: EdgeInsets.only(
+                    top: appHeight / 20,
+                    left: appWidth / 8,
+                    right: appWidth / 8),
               ),
-              margin: EdgeInsets.only(
-                  top: appHeight / 20, left: appWidth / 8, right: appWidth / 8),
-            ),
-            Container(
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return AccountDetails();
-                      },
-                    ),
-                  );
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(appHeight / 50),
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: appHeight / 200,
-                          offset: Offset(appHeight / 200, appWidth / 140),
-                        ),
-                      ]),
-                  margin: EdgeInsets.only(top: appHeight / 10),
-                  padding: EdgeInsets.only(
-                      top: appHeight / 30,
-                      bottom: appHeight / 30,
-                      left: appWidth / 18,
-                      right: appWidth / 18),
-                  width: appWidth / 1.2,
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        child: Image(
-                          image: AssetImage("images/seed.png"),
-                          width: appWidth / 5,
-                        ),
+              Container(
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return AccountDetails();
+                        },
                       ),
-                      Container(
-                        margin: EdgeInsets.only(left: appWidth / 15),
-                        child: Column(
-                          children: <Widget>[
-                            Container(
-                              child: Text(
-                                "12 Word Seed",
-                                style: TextStyle(
-                                  fontSize: appWidth / 15,
-                                  fontWeight: FontWeight.w600,
+                    );
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(appHeight / 50),
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: appHeight / 200,
+                            offset: Offset(appHeight / 200, appWidth / 140),
+                          ),
+                        ]),
+                    margin: EdgeInsets.only(top: appHeight / 10),
+                    padding: EdgeInsets.only(
+                        top: appHeight / 30,
+                        bottom: appHeight / 30,
+                        left: appWidth / 18,
+                        right: appWidth / 18),
+                    width: appWidth / 1.2,
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          child: Image(
+                            image: AssetImage("images/seed.png"),
+                            width: appWidth / 5,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(left: appWidth / 15),
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                child: Text(
+                                  "12 Word Seed",
+                                  style: TextStyle(
+                                    fontSize: appWidth / 15,
+                                    fontWeight: FontWeight.w600,
+                                  ),
                                 ),
                               ),
-                              
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-            Container(
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return PkeyPage();
-                      },
-                    ),
-                  );
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(appHeight / 50),
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: appHeight / 200,
-                          offset: Offset(appHeight / 200, appWidth / 140),
-                        ),
-                      ]),
-                  margin: EdgeInsets.only(top: appHeight / 10),
-                  padding: EdgeInsets.only(
-                      top: appHeight / 30,
-                      bottom: appHeight / 30,
-                      left: appWidth / 18,
-                      right: appWidth / 18),
-                  width: appWidth / 1.2,
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        child: Image(
-                          image: AssetImage("images/key.png"),
-                          width: appWidth / 5,
-                        ),
+              Container(
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return PkeyPage();
+                        },
                       ),
-                      Container(
-                        margin: EdgeInsets.only(left: appWidth / 15),
-                        child: Column(
-                          children: <Widget>[
-                            Container(
-                              child: Text(
-                                "Private Key",
-                                style: TextStyle(
-                                  fontSize: appWidth / 13,
-                                  fontWeight: FontWeight.w600,
+                    );
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(appHeight / 50),
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: appHeight / 200,
+                            offset: Offset(appHeight / 200, appWidth / 140),
+                          ),
+                        ]),
+                    margin: EdgeInsets.only(top: appHeight / 10),
+                    padding: EdgeInsets.only(
+                        top: appHeight / 30,
+                        bottom: appHeight / 30,
+                        left: appWidth / 18,
+                        right: appWidth / 18),
+                    width: appWidth / 1.2,
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          child: Image(
+                            image: AssetImage("images/key.png"),
+                            width: appWidth / 5,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(left: appWidth / 15),
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                child: Text(
+                                  "Private Key",
+                                  style: TextStyle(
+                                    fontSize: appWidth / 13,
+                                    fontWeight: FontWeight.w600,
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-            Container(
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return CreateNewAccount();
-                      },
-                    ),
-                  );
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(appHeight / 50),
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: appHeight / 200,
-                          offset: Offset(appHeight / 200, appWidth / 140),
-                        ),
-                      ]),
-                  margin: EdgeInsets.only(top: appHeight / 10),
-                  padding: EdgeInsets.only(
-                      top: appHeight / 30,
-                      bottom: appHeight / 30,
-                      left: appWidth / 18,
-                      right: appWidth / 18),
-                  width: appWidth / 1.2,
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        child: Image(
-                          image: AssetImage("images/new_account.png"),
-                          width: appWidth / 5,
-                        ),
+              Container(
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return CreateNewAccount();
+                        },
                       ),
-                      Container(
-                        margin: EdgeInsets.only(left: appWidth / 20),
-                        child: Column(
-                          children: <Widget>[
-                            Container(
-                              child: Text(
-                                "Create Account",
-                                style: TextStyle(
-                                  fontSize: appWidth / 16,
-                                  fontWeight: FontWeight.w600,
+                    );
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(appHeight / 50),
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: appHeight / 200,
+                            offset: Offset(appHeight / 200, appWidth / 140),
+                          ),
+                        ]),
+                    margin: EdgeInsets.only(top: appHeight / 10),
+                    padding: EdgeInsets.only(
+                        top: appHeight / 30,
+                        bottom: appHeight / 30,
+                        left: appWidth / 18,
+                        right: appWidth / 18),
+                    width: appWidth / 1.2,
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          child: Image(
+                            image: AssetImage("images/new_account.png"),
+                            width: appWidth / 5,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(left: appWidth / 20),
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                child: Text(
+                                  "Create Account",
+                                  style: TextStyle(
+                                    fontSize: appWidth / 16,
+                                    fontWeight: FontWeight.w600,
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
+            ],
+          ),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Color(0xffb2f78fb), Color(0xffb5c53d4)],
             ),
-          ],
-        ),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Color(0xffb2f78fb), Color(0xffb5c53d4)],
           ),
         ),
       ),
